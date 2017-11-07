d3301fadfda87eedbfa0ae0be0650ab2f11176fc
whatbeg <huqiu00@163.com>
2017-11-07 11:35:10 +0800

    add GradientBoostingExample to test executor skew issue

---
+++ src/main/scala/examples/GradientBoosting/GradientBoostingExample.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Authors NJU PASA BigData Laboratory. Qiu Hu. huqiu00#163.com
+ */
+package examples.GradientBoosting
+
+import datasets.UCI_adult
+import org.apache.spark.ml.classification.GBTClassifier
+import org.apache.spark.ml.evaluation.{Evaluator, MulticlassClassificationEvaluator}
+import org.apache.spark.ml.feature.IndexToString
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.utils.engine.Engine
+
+object GradientBoostingExample {
+  def main(args: Array[String]): Unit = {
+
+    import Utils._
+
+    val spark = SparkSession.builder()
+      .appName(this.getClass.getSimpleName)
+            .master("local[*]")
+      .getOrCreate()
+
+    val parallelism = Engine.getParallelism(spark.sparkContext)
+    println(s"Create Spark Context Succeed! Parallelism is $parallelism")
+    spark.conf.set("spark.default.parallelism", parallelism)
+    spark.conf.set("spark.locality.wait.node", 0)
+
+    trainParser.parse(args, TrainParams()).map(param => {
+
+      spark.sparkContext.setLogLevel(param.debugLevel)
+
+      val trainingData = new UCI_adult().load_data(spark, param.trainFile, param.featuresFile, 1, parallelism)
+      //        .repartition(parallelism)
+      val testData = new UCI_adult().load_data(spark, param.testFile, param.featuresFile, 1, parallelism)
+      //        .repartition(parallelism)
+
+      val gbt = new GBTClassifier()
+        .setMaxIter(param.numIteration)
+        .setMaxBins(param.maxBins)
+        .setLabelCol("label")
+        .setFeaturesCol("features")
+
+      val model = gbt.fit(trainingData)
+      // Evaluate model on test instances and compute test error
+      val predictions = model.transform(testData)
+      predictions.select("prediction", "label", "features").show(5)
+
+      val accuracy = Evaluator.evaluatePrediction(predictions)
+      println("Test Accuracy = " + accuracy)
+      if (param.idebug) println("Learned classification GBT model:\n" + model.toDebugString)
+      model
+    })
+    spark.stop()
+  }
+}
--- src/main/scala/org/apache/spark/ml/evaluation/Evaluator.scala
+++ src/main/scala/org/apache/spark/ml/evaluation/Evaluator.scala
@@ -21,9 +21,8 @@ abstract class Evaluator {
 
 object Evaluator {
 
-  case class Record(instance: Long, label: Double, features: Vector)
   /**
-    * Evaluates model output and returns a Accuracy metric.
+    * Evaluates model output (instance: Long, label: Double, features: Vector) and returns a Accuracy metric.
     *
     * @param dataset dataset to evaluate
     * @return metric
@@ -41,6 +40,22 @@ object Evaluator {
       val features = row.getAs[Vector]("features")
       val label = row.getAs[Double]("label")
       if (features.argmax == label.toInt) rightCount.add(1)
+      totalCount.add(1)
+    }
+    new Accuracy(rightCount.value, totalCount.value)
+  }
+
+  def evaluatePrediction(dataset: Dataset[_]): Accuracy = {
+    val schema = dataset.schema
+    val schema_string = schema.toArray.map { sch => sch.name }
+    require(schema_string.contains("label"), "DataSet has no label col")
+    require(schema_string.contains("prediction"), "DataSet has no prediction col")
+    val totalCount = dataset.sparkSession.sparkContext.doubleAccumulator("totalCount")
+    val rightCount = dataset.sparkSession.sparkContext.doubleAccumulator("rightCount")
+    dataset.rdd.foreach { case (row: Row) =>
+      val predict = row.getAs[Double]("prediction")
+      val label = row.getAs[Double]("label")
+      if (predict.toInt == label.toInt) rightCount.add(1)
       totalCount.add(1)
     }
     new Accuracy(rightCount.value, totalCount.value)
